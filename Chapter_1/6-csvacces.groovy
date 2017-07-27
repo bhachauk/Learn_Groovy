b78efb1b271f2f54f7892eecef486d40e5864055
Bhanu chander U <bhanuchander210@gmail.com>
2017-07-27 20:18:15 +0530

    Create 6-csvacces.groovy

---
+++ Chapter_1/6-csvacces.groovy
@@ -0,0 +1,10 @@
+@Grab('com.xlson.groovycsv:groovycsv:1.1')
+import static com.xlson.groovycsv.CsvParser.parseCsv
+def csv1="output.csv"
+df = new FileReader(csv1)
+def stringWriter = new StringWriter()
+for (line in parseCsv (df))
+{
+	
+	println "$line.Title $line.Author $line.Genre"
+}
