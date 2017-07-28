7134337a9ae5326b8f693dcd1ed2ee797a2e12f9
Bhanu chander U <bhanuchander210@gmail.com>
2017-07-28 12:45:13 +0530

    Create 7-maplistxml.groovy

---
+++ Chapter_1/7-maplistxml.groovy
@@ -0,0 +1,36 @@
+@Grab('com.xlson.groovycsv:groovycsv:1.1')
+import static com.xlson.groovycsv.CsvParser.parseCsv
+import groovy.xml.MarkupBuilder
+def csv="srce.csv"
+int i=1;
+value=[]
+out=[]
+cont=[]
+att=("att")
+id="id"
+df = new FileReader(csv)
+def writer = new StringWriter()
+def data = parseCsv(df, readFirstLine: true)
+new MarkupBuilder(writer).root {
+    data.eachWithIndex { row,id->
+        row.values.eachWithIndex{k,idx ->
+            if(i<=3){
+
+                value[idx]=(k)
+
+            }else{
+                cont[idx]=(k)
+                //"Row"(k)
+            }
+        i=i+1;
+        
+
+        }
+    j=value    
+    def mp=[value,cont].transpose().collectEntries { it }
+    "Row"(mp)
+    }
+}
+def f = new File("attr.xml")
+println writer.toString()
+f.write (writer.toString())
