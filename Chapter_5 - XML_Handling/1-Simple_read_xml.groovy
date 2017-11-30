ffcf5bbb014812d790f88662a69b49849fd40c9d
Bhanu chander U <bhanuchander210@gmail.com>
2017-11-30 23:28:04 +0530

    Update 1-Simple_read_xml.groovy

--- Chapter_5 - XML_Handling/1-Simple_read_xml.groovy
+++ Chapter_5 - XML_Handling/1-Simple_read_xml.groovy
@@ -1,9 +1,19 @@
 // Init_ Reading xml file
-
+// XmlSlurper or XmlParser both works
 def root
 
 // Method_1 :       
 root = new XmlSlurper().parse(new File(filename))
 
 // Method_2 :
-root = new XmlSlurper().parseText(versiontag.toString()+new File(filename).getText())
+root = new XmlSlurper().parseText(new File(filename).getText())
+
+//Method_3 :
+def xmltext = '''
+    <list>
+        <technology>
+            <name>Groovy</name>
+        </technology>
+    </list>
+'''
+root = new XmlSlurper().parseText(xmltext)
