0f1a2b9d3b6f67c4e0a6f477c6dd15c3790c6cf0
Bhanu chander U <bhanuchander210@gmail.com>
2017-11-30 19:49:33 +0530

    Update 1-Hello.groovy

--- Chapter_1/1-Hello.groovy
+++ Chapter_1/1-Hello.groovy
@@ -1,2 +1,12 @@
-//don't forget the first step ever. :D 
-println ("Hello world")
+//don't forget the first step ever. :D
+// `println` prints in newline
+println ("Hello World")
+
+//Ordinary print
+print "Hello World"
+
+//Make define and print
+String string1 = "Hello"
+String string2 = "World"
+
+print string1+' '+string2
