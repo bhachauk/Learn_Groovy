5e7bf01c12189595c034345b9bcf5df2e5b3cc71
Bhanu chander U <bhanuchander210@gmail.com>
2017-12-01 19:42:45 +0530

    Update sublist.groovy

--- chapter_3/sublist.groovy
+++ chapter_3/sublist.groovy
@@ -1,21 +1,26 @@
 import java.util.*;
 
-   // create two array list objects       
-   List arrlistsrc = new ArrayList();
-   List arrlisttarget = new ArrayList();
+// create two array list objects       
+List arrlistsrc = new ArrayList();
+List arrlisttarget = new ArrayList();
       
-   // populate two lists
-   arrlistsrc.add("A");
-   arrlistsrc.add("B");
-   arrlistsrc.add("C");
-   arrlistsrc.add("D");
-   arrlistsrc.add("E"); 
-      
-   arrlisttarget.add("C");
-   arrlisttarget.add("D");
-   arrlisttarget.add("E");
+// populate two lists
+arrlistsrc.add("A");
+arrlistsrc.add("B");
+arrlistsrc.add("C");
+arrlistsrc.add("D");
+arrlistsrc.add("E"); 
+
+arrlisttarget.add("C");
+arrlisttarget.add("D");
+arrlisttarget.add("E");
            
-   // check target list in source list
-   int index = Collections.indexOfSubList(arrlistsrc, arrlisttarget);
+// check target list in source list
+int index = Collections.indexOfSubList(arrlistsrc, arrlisttarget);
       
-   System.out.println("Target list starts at index: "+index);    
+System.out.println("Target list starts at index: "+index);    
+
+/*
+Output : 
+2
+*/
