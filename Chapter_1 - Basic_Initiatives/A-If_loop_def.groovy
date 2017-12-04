5fa879f17edfd5c4bd6ee9ef6e0b8b6f027d49e9
Bhanu chander U <bhanuchander210@gmail.com>
2017-12-04 12:09:42 +0530

    Create A-If_loop_def.groovy

---
+++ Chapter_1 - Basic_Initiatives/A-If_loop_def.groovy
@@ -0,0 +1,11 @@
+boolean i= true
+if (i)
+msg = "'If' check passed" // Default message to send
+else 
+msg = "'Else' check passed "    
+println "MESSAGE: "+msg
+
+/*
+INFO:
+Here `msg` variable assigned without any global `def` and working as global if applied inside in `if`-`else` Loop
+*/
