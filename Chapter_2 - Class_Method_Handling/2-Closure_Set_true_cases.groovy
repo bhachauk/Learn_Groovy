7a795d34cefc468fcb08e6875d5428896a38bb98
Bhanu chander U <bhanuchander210@gmail.com>
2018-01-04 15:52:16 +0530

    Create 2-Closure_Set_true_cases.groovy

---
+++ Chapter_2 - Class_Method_Handling/2-Closure_Set_true_cases.groovy
@@ -0,0 +1,11 @@
+//--------------------------------------------------------------//
+//----------------------Set your true cases---------------------//
+//--------------------------------------------------------------//
+
+boolean isTrue(def it)
+{ 
+	return it==1.toInteger() || it.toLowerCase().contains('y')|| it.toLowerCase().contains('true') || it==true
+}
+
+
+// Note : Return `true` or `false` .
