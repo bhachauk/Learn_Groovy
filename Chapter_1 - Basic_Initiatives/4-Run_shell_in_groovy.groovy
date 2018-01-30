bdbd4884ac2d53c259b3b668dbd45eadea6a0d00
Bhanu chander U <bhanuchander210@gmail.com>
2018-01-30 15:30:56 +0530

    Update 4-Run_shell_in_groovy.groovy

--- Chapter_1 - Basic_Initiatives/4-Run_shell_in_groovy.groovy
+++ Chapter_1 - Basic_Initiatives/4-Run_shell_in_groovy.groovy
@@ -1,3 +1,14 @@
-#groovy
+//groovy
 def x= ['/bin/sh','-c',/grep -o 'OSI Alarm : Resynchronization ...' in1.txt | wc -l/].execute().text.trim()
 println x
+
+// Meta class with shell command execution.
+List.metaClass.cmdexecute
+{  List inp->
+   println inp.execute().text
+}
+
+void execute (List cmd)
+{
+  println cmd.execute().text
+}
