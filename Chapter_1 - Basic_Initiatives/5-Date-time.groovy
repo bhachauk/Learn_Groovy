5fae25598719f20b1711d6f03d7ce2882765e017
Bhanu chander U <bhanuchander210@gmail.com>
2018-02-23 20:20:16 +0530

    Update 5-Date-time.groovy

--- Chapter_1 - Basic_Initiatives/5-Date-time.groovy
+++ Chapter_1 - Basic_Initiatives/5-Date-time.groovy
@@ -11,3 +11,17 @@ Date edate = Date.parse( 'dd_mm_yyyy', endDate )
 String curr_date=it.format('dd_mm_yyyy')
 print curr_date
 }
+
+//------------------------------------------------
+// Go to the nth of the date.
+
+Date future_of(Date current,int a)
+{
+  1.upto(a)
+	 {
+		  current=current.next()	
+	 }
+	 return current
+}
+
+println future_of(sdate,10)
